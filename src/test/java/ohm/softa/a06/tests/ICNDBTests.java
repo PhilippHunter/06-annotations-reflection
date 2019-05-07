464e69a945aee82ef3fc590afef1881b482f46d1
PhilippHunter <philippjaeger98@web.de>
2019-05-07 14:05:51 +0200

    added more test cases

--- src/test/java/ohm/softa/a06/tests/ICNDBTests.java
+++ src/test/java/ohm/softa/a06/tests/ICNDBTests.java
@@ -1,13 +1,21 @@
 package ohm.softa.a06.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ohm.softa.a06.ICNDBApi;
 import ohm.softa.a06.model.Joke;
+import ohm.softa.a06.model.JokeAdapter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -18,7 +26,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ICNDBTests {
 
 	private static final Logger logger = LogManager.getLogger(ICNDBTests.class);
-	private static final int REQUEST_COUNT = 10;
+	private static final int REQUEST_COUNT = 20;
+
+	private ICNDBApi norrisApi;
+
+	@BeforeEach
+	void setup() {
+		Gson gson = new GsonBuilder()
+			.registerTypeAdapter(Joke.class, new JokeAdapter())
+			.create();
+
+		Retrofit retrofit = new Retrofit.Builder()
+			.baseUrl("http://api.icndb.com")
+			.addConverterFactory(GsonConverterFactory.create(gson))
+			.build();
+
+		norrisApi = retrofit.create(ICNDBApi.class);
+	}
 
 	@Test
 	void testCollision() throws IOException {
@@ -28,12 +52,15 @@ class ICNDBTests {
 
 		while (requests++ < REQUEST_COUNT) {
 			// TODO Prepare call object
+			Call<Joke> jokeCall = norrisApi.getRandomJoke();
 
 			// TODO Perform a synchronous request
+			Response<Joke> jokeResponse = jokeCall.execute();
 
 			// TODO Extract object
+			if(!jokeResponse.isSuccessful()) continue;
 
-			Joke j = null;
+			Joke j = jokeResponse.body();
 
 			if(jokeNumbers.contains(j.getNumber())) {
 				logger.info(String.format("Collision at joke %s", j.getNumber()));
@@ -46,5 +73,27 @@ class ICNDBTests {
 		}
 
 		assertTrue(collision, String.format("Completed %d requests without collision; consider increasing REQUEST_COUNT", requests));
+	}
+
+	@Test
+	void howLongTilCollision() throws IOException {
+		int counter = 0;
+		boolean collision = false;
+		List<Integer> jokeIndexes = new LinkedList<>();
+
+		while(!collision) {
+			Joke j = norrisApi.getRandomJoke().execute().body();
+
+			if(jokeIndexes.contains(j.getNumber())) {
+				collision = true;
+				logger.info(j.toString());
+				break;
+			}
+
+			jokeIndexes.add(j.getNumber());
+			counter++;
+		}
+
+		logger.info(String.format("Completed %d requests until the first collision appeared", counter));
 	}
 }
